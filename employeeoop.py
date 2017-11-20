a52a37a1e84b28abf3acda5d95693795ae1dd241
tahmed5 <31918795+tahmed5@users.noreply.github.com>
2017-11-20 21:10:35 +0000

    Add files via upload

---
+++ employeeoop.py
@@ -0,0 +1,88 @@
+import datetime
+
+class Employee:
+
+    num_of_emps = 0
+    raise_amt = 1.04
+    
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+        Employee.num_of_emps += 1
+        
+    def fullname(self):
+        print(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)
+
+    @classmethod
+    def set_raise_amt(cls,amount):
+        cls.raise_amt = amount
+
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first, last, pay)
+    
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
+    
+class Developer(Employee):
+    raise_amt = 1.10
+    
+    def __init__(self, first, last, pay, prog_lang):
+        super().__init__(first,last,pay)
+        self.prog_lang = prog_lang
+
+class Manager(Employee):
+    def __init__(self, first, last, pay, employees= None):
+        super().__init__(first,last,pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_emp(self,emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+            
+    def remove_emp(self,emp):
+        if emp not in self.employees:
+            self.employees.remove(emp)
+
+    def print_emps(self):
+        for emp in self.employees:
+            print(emp.fullname())
+        
+dev_1 = Developer('Corey', 'Schafer', 50000, 'C++')
+dev_2 = Developer('Test', 'Employee', 60000, 'Python')
+
+mgr_1 = Manager('Sue', 'Smith', 90000, [dev_1])
+mgr_1.add_emp(dev_2)
+
+print(mgr_1.print_emps())
+
+emp_1 = Employee('James', 'Conway', 50000)
+emp_2 = Employee('Barack', 'Obama', 10000)
+
+
+'''
+dev_1.apply_raise()
+print(dev_1.prog_lang)
+
+my_date = datetime.date(2017, 11 , 20)
+print(Employee.is_workday(my_date))
+
+
+emp_str_1 = 'John-Doe-70000'
+
+new_emp_1 = Employee.from_string(emp_str_1)
+print(new_emp_1.email)
+Employee.set_raise_amt(1.1)
+'''
